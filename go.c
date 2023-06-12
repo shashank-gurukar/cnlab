f1dcd0bd511edf28faed8490af65a76b7cf928e1
shashank-gurukar <shashankpgurukar@gmail.com>
2023-06-12 17:17:26 +0530

    ,,,

--- go.c
+++ go.c
@@ -1,51 +1,41 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<time.h>
 
-int n,r;
-struct frame
+void transmission(int i,int n,int tf)
 {
-        char ack;
-        int data;
-}frm[10];
-
-
-void sender()
-{
-        int i;
-        printf("\nEnter the no. of packets to be sent:");
-        scanf("%d",&n);
-        for(i=1;i<=n;i++)
+        while (i <= tf)
         {
-                printf("\nEnter data for packets[%d]: ",i);
-                scanf("%d",&frm[i].data);
-                frm[i].ack='y';
+                int z = 0,k;
+                for (k = i; k < i + n && k <= tf; k++)
+                        printf("Sending frame %d\n",k);
+                for (k = i; k < i + n && k <= tf; k++)
+                {
+                        int f = rand() % 4;
+                        if (!f)
+                        {
+                                printf("Acknowledgement for frame %d\n",k);
+                                z++;
+                        }
+                        else
+                        {
+                                printf("Frame %d not received\n",k);
+                                printf("Retransmitting\n");
+                                break;
+                        }
+                }
+                printf("\n");
+                i=i+z;
         }
 }
-void recvack()
-{
-        int i;
-        r=rand()%n;
-        frm[r].ack='n';
-        for(i=1;i<=n;i++)
-        {
-                if(frm[i].ack=='n')
-                        printf("\nThe packet number %d is not received\n",r);
-                else
-                        printf("\nThe packet number %d is recieved having data %d\n",i,frm[i].data);
-        }
-}
-void resend()
-{
-        printf("\nResending packet %d",r);
-        sleep(2);
-        frm[r].ack='y';
-        printf("\nThe received packet is %d having data %d",r,frm[r].data);
-}
+
 int main()
 {
-        sender();
-        recvack();
-        resend();
-        printf("\nAll packets sent successfully\n");
+        int tf,n,i=1;
+        srand(time(NULL));
+        printf("Enter the total number of frames\n");
+        scanf("%d",&tf);
+        printf("Enter the window size\n");
+        scanf("%d",&n);
+        transmission(i,n,tf);
         return 0;
 }
